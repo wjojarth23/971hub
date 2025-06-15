403be88700309e02580d8da6e08bb2ac5328a3fd
wjojarth23 <1000034323@mvla.net>
2025-06-14 22:59:24 -0700

    good i hope

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,6 @@
 <script>
   import '../app.css';
-  import { page } from '$app/stores';
-  import { FileText, List, Settings } from 'lucide-svelte';
 </script>
-
-<div class="header">
-  <div class="container">
-    <nav class="nav">
-      <div class="nav-brand">Manufacturing Management</div>      <ul class="nav-links">
-        <li>
-          <a href="/" class="nav-link" class:active={$page.url.pathname === '/'}>
-            <List size={16} />
-            Parts List
-          </a>
-        </li>
-        <li>
-          <a href="/create" class="nav-link" class:active={$page.url.pathname === '/create'}>
-            <FileText size={16} />
-            Create Part
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </div>
-</div>
 
 <main class="container">
   <slot />
